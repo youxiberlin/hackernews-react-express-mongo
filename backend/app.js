358d2cddc8b8aad6ef03d35ee75cb05ef37dc9f7
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-24 15:30:49 +0200

    Add postStory controller

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { port, mongoRoute } = require('./config');
 const { initializeMongoDB, insertStory, insertTopStories } = require('./services/mongodb');
 const logger = require('./services/logger');
-const { getTopStories } = require('./controllers');
+const { getTopStories, postStory } = require('./controllers');
 
 const app = express();
 app.use(cors());
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('hello world'));
 app.get('/topStories', getTopStories)
+app.post('/story', postStory);
 app.listen(port, () => logger.info(`App listening at port ${port}`));
 initializeMongoDB(mongoRoute)
   // .then(() => insertTopStories())

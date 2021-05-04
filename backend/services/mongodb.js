bbd4da4c7c2047d5089b1c5ce0364324f4fc375a
Yuki Sato <yuki.sato@lightcurve.io>
2021-05-04 18:23:48 +0200

    Add controllers

--- backend/services/mongodb.js
+++ backend/services/mongodb.js
@@ -10,33 +10,6 @@ const connect = (mongoRoute) => mongoose.connect(mongoRoute, {
     useCreateIndex: true
 });
 
-const getStory = async () => {
-    try {
-      const { data } = await axios.get('https://hacker-news.firebaseio.com/v0/item/8863.json');
-      return data;
-    } catch (e) {
-      console.log(e);
-    }
-};
-
-const getTopStories = async () => {
-  try {
-    const { data: topStories } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
-    const catchErr = p => p.catch(err => {
-      console.log('error', err);
-      return { data: {}};
-    });
-    const getItemPromises = topStories
-      .slice(0, 30)
-      .map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`))
-      .map(promise => catchErr(promise))
-    const items = await Promise.all(getItemPromises);
-    const itemsData = items.map(item => item.data);
-    return itemsData;
-  } catch (e) {
-    console.log(e);
-  }
-};
 
 const insertStory = async () => {
   const data = await getStory();
